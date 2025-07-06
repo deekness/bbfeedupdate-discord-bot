b61030c4485f955d2eecb086ddc32afdcc8cbdda
deekness <152800135+deekness@users.noreply.github.com>
2025-07-06 10:55:44 -0500

    Update bb_bot.py

--- bb_bot.py
+++ bb_bot.py
@@ -2498,7 +2498,6 @@ class BBDatabase:
 
 class BBDiscordBot(commands.Bot):
     """Main Discord bot class with 24/7 reliability features"""
-tree = app_commands.CommandTree(bot)
     
     def __init__(self):
         self.config = Config()
