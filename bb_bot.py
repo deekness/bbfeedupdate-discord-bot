ae6e304a52745263eac32824f5eae25c8e6b25f8
deekness <152800135+deekness@users.noreply.github.com>
2025-07-14 15:43:07 -0500

    Update bb_bot.py

--- bb_bot.py
+++ bb_bot.py
@@ -765,12 +765,19 @@ class UnifiedContentMonitor:
         # Configure monitored Bluesky accounts
         self.monitored_accounts = [
             # Major BB News/Update Accounts
-            "bigbrothernetwork.bsky.social‬",
-            "realitytvwrapped.bsky.social", 
+            "bigbrothernetwork.bsky.social",
             "bbscreencaps.bsky.social",
             "hamsterwatch.bsky.social",
-            "onlinebigbrother.bsky.social",
-            "realitysteve.bsky.social",
+            "pooyaism.bsky.social‬",
+            "hamsterwatch.com",
+            "thebbpresident.bsky.social‬",
+            "bblionotev.bsky.social‬",
+            "bbliveupdaters.bsky.social‬‬",
+            "toomsbb.bsky.social‬‬‬",
+            "bbteamnorth.bsky.social‬‬‬‬",
+            "bbnutters.bsky.social‬‬‬‬‬",
+
+            
             
             # Add more accounts as you discover them
             # You can add individual contestant accounts if they join Bluesky
